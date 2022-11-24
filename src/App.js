019c5d161cb208304ab30c3c1986b37e4e487266
unknown <sifatniloy18@gmail.com>
2022-11-24 11:01:22 -0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div >
+      <Navbar></Navbar>
     </div>
   );
 }
