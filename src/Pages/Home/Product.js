8ef64042baf57496f7773a79a7ff603acbc762e9
unknown <sifatniloy18@gmail.com>
2022-11-26 14:04:39 -0600

    small errors from console fixed

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Product = ({product}) => {
     const {name, img, description}= product;
     return (
-        <div class="card w-96 bg-base-100 shadow-xl  bg-green-100">
-            <figure class="px-10 pt-10">
-                <img src={img} alt="Shoes" class="rounded-xl" />
+        <div className="card w-96 bg-base-100 shadow-xl  bg-green-100">
+            <figure className="px-10 pt-10">
+                <img src={img} alt="Shoes" className="rounded-xl" />
             </figure>
-            <div class="card-body items-center text-center">
-                <h2 class="card-title">{name}</h2>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions">
-                    <button class="btn btn-primary">Buy Now</button>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Buy Now</button>
                 </div>
             </div>
         </div>
