158114f6ef9408b03a5dcbb855dc76889c08ece5
unknown <sifatniloy18@gmail.com>
2022-11-27 06:31:01 -0600

    camera product card responsive done

--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -8,9 +8,11 @@ const Products = () => {
     return (
         <div>
             <h2>This is products</h2>
-            {
-                products.map(product=> <Product key={product._id} product={product}></Product>)
-            }
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    products.map(product => <Product key={product._id} product={product}></Product>)
+                }
+           </div>
         </div>
     );
 };
