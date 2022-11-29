df9305768f814f15a808f3ae2a01e89d55468838
unknown <sifatniloy18@gmail.com>
2022-11-29 08:34:49 -0600

    product fetch from mongodb

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -9,7 +9,7 @@ const Product = ({product}) => {
             </figure>
             <div className="card-body items-center text-center">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>{description}</p>
                 <div className="card-actions">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
