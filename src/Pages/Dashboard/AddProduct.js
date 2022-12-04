c83bd2e39da4b89832d586f64292822e14d2825f
unknown <sifatniloy18@gmail.com>
2022-12-04 13:43:34 -0600

    add product form added

--- src/Pages/Dashboard/AddProduct.js
+++ src/Pages/Dashboard/AddProduct.js
@@ -1,9 +1,116 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddProduct = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const onSubmit = async data => {
+        console.log(data);
+       
+    };
     return (
         <div>
             <h2 className="3xl">Add a product</h2>
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+                <div className="form-control w-full max-w-xs">
+                   
+                    <input
+                        type="text"
+                        placeholder="product name"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'name is required',
+                            }
+
+                        })}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <p role="alert"><span className="label-text-alt text-red-600">{errors.name.message}</span></p>}
+                        
+
+
+                    </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    
+                    <input
+                        type="text"
+                        placeholder="product price"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'name is required',
+                            }
+
+                        },
+                            )}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <p role="alert"><span className="label-text-alt text-red-600">{errors.name.message}</span></p>}
+                        
+
+
+                    </label>
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    
+
+                    <input
+                        type="text"
+                        placeholder="description"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("name", {
+                            required: {
+                                value: true,
+                                message: 'name is required',
+                            }
+
+                        })}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <p role="alert"><span className="label-text-alt text-red-600">{errors.name.message}</span></p>}
+                        
+
+
+                    </label>
+                </div>
+
+
+                
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">upload image</span>
+
+                    </label>
+                    <input
+                        type="file"
+                        placeholder="your password"
+                        className="input input-bordered w-full max-w-xs"
+                        {...register("image", {
+                            required: {
+                                value: true,
+                                message: 'Image is required',
+                            }
+
+                        },
+                            )}
+                    />
+                    <label className="label">
+                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}
+                        </span>}
+                        
+                    </label>
+                </div>
+
+
+
+            
+
+                <input className='btn w-full max-w-xs text-white' type="submit" value="Add Product" />
+            </form>
         </div>
     );
 };
--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 
 const ManageAllOrders = () => {
+    const [orders, setOrders] = useState([]);
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/allorders`, {
+                method: 'GET',
+                headers: {
+                    'authorization': `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            })
+                .then(res => {
+                    // console.log('res', res);
+                    if (res.status === 401 || res.status === 403) {
+                        // signOut(auth);
+                        localStorage.removeItem('accessToken');
+                        navigate('/');
+                    }
+
+                    return res.json()
+                })
+                .then(data => {
+                    setOrders(data)
+                })
+        }
+
+    }, [user]);
     return (
-        <div>
-            <h2 className='text-4xl'>manage orders</h2>
+        <div className=''>
+            <h1 className='text-2xl'>Total Ordered Products: {orders.length} </h1>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Phone</th>
+                            <th>Delivery Address</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {
+                            orders.map((order, index) => <tr>
+                                <th>{index + 1}</th>
+                                <th>{order.name}</th>
+                                <td>{order.price}</td>
+                                <td>{order.phone}</td>
+                                <td>{order.address}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
