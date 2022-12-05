92ff7f5271e7f848ca4679358e0a9772925c961e
unknown <sifatniloy18@gmail.com>
2022-12-05 00:50:51 -0600

    rating star added in review

--- src/Pages/Dashboard/Review.js
+++ src/Pages/Dashboard/Review.js
@@ -4,6 +4,15 @@ const Review = () => {
     return (
         <div className='flex justify-center'>
             <h1>Review</h1>
+            <div>
+                <div class="rating">
+                    <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                    <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" checked />
+                    <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                    <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                    <input type="radio" name="rating-4" class="mask mask-star-2 bg-green-500" />
+                </div>
+            </div>
         </div>
     );
 };
