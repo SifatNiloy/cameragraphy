4b1b7de65298c14bff595e682e705e880f95dbd3
SifatNiloy <SifatNiloy18@gmail.com>
2024-03-13 23:22:32 +0600

    product style added

--- src/Pages/Home/Product.js
+++ src/Pages/Home/Product.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Product = ({ product }) => {
   const { _id, name, price, img, description } = product;
@@ -9,22 +9,24 @@ const Product = ({ product }) => {
     navigate(`/product/${_id}`);
   };
 
-  // Function to truncate the description
   const truncateDescription = (text, maxLength) => {
-    return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
+    return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
   };
 
-  // Set the maximum length for the truncated description
   const maxDescriptionLength = 100;
 
   return (
-    <div className="max-w-md mx-auto bg-white rounded-lg overflow-hidden shadow-lg my-8">
+    <div className="max-w-md mx-auto bg-gray-100 rounded-md overflow-hidden shadow-lg my-8 transition duration-300 transform hover:scale-105 hover:shadow-2xl hover:bg-gray-200">
       <div className="relative overflow-hidden">
-        <img className="w-full h-50 object-cover" src={img} alt={name} />
+        <img
+          className="w-full h-50 object-cover rounded-t-md"
+          src={img}
+          alt={name}
+        />
       </div>
       <div className="p-4">
-        <h2 className="text-xl font-semibold mb-2">{name}</h2>
-        <p className="text-gray-500 text-sm mb-4">
+        <h2 className="text-xl font-semibold mb-2 text-gray-800">{name}</h2>
+        <p className="text-gray-600 text-sm mb-4">
           {truncateDescription(description, maxDescriptionLength)}
         </p>
         <div className="flex justify-between items-center">
@@ -32,7 +34,7 @@ const Product = ({ product }) => {
             <p className="text-blue-500 font-bold">Price: {price}</p>
           </div>
           <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 transform hover:scale-105 focus:outline-none focus:shadow-outline"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 focus:outline-none focus:shadow-outline"
             onClick={navigateToPurchase}
           >
             Buy Now
--- src/Pages/Home/Products.js
+++ src/Pages/Home/Products.js
@@ -8,6 +8,7 @@ const Products = () => {
 
   return (
     <div className="container mx-auto mt-10">
+        <h2 className="text-4xl text-center font-semibold mb-8">Latest Products</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <Product key={product._id} product={product} />
