71048cb812164462d72e2bb0ed660d702e4d6231
SifatNiloy <sifatniloy18@gmail.com>
2024-07-08 20:42:30 +0600

    article component made better

--- src/Pages/Home/Article.js
+++ src/Pages/Home/Article.js
@@ -3,15 +3,15 @@ import React from 'react';
 const Article = () => {
   return (
     <div className="p-6 bg-gray-100">
-      <h2 className="text-4xl mt-8 mb-6 text-orange-700 font-bold">Explore the World of Photography</h2>
-      <div className="text-gray-700 font-serif">
-        <p className="mb-4">
+      <h2 className="text-3xl sm:text-4xl mt-8 mb-6 text-orange-600 font-semibold leading-tight">Explore the World of Photography</h2>
+      <div className="text-gray-800 font-serif">
+        <p className="mb-4 text-lg">
           Unleash your creativity and capture timeless memories with our exclusive range of Digital, DSLR, and action cameras. Whether you're a beginner, enthusiast, or professional, we have the perfect camera for you.
         </p>
-        <p className="mb-4">
+        <p className="mb-4 text-lg">
           Discover a diverse selection of cameras from world-class brands such as Canon, Nikon, Sony, and Samsung. Our collection includes responsive Canon digital cameras, budget-friendly Nikon DSLRs, and versatile Sony digital cameras, each designed to meet your unique photography needs.
         </p>
-        <p className="mb-4">
+        <p className="mb-4 text-lg">
           Explore the precision and convenience offered by Sony digital cameras, providing simplicity and ease of use with a fixed lens. Find joy in capturing precious moments with Nikon COOLPIX cameras, offering quality and affordability. Check out the enticing Canon DSLR camera prices, making high-quality photography accessible to all.
         </p>
       </div>
